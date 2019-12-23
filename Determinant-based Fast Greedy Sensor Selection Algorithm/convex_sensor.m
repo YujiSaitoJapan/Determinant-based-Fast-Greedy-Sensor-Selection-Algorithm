18254ae840a216fae99a8d6bad6a97e596cebea5
YujiSaitoJapan <57979724+YujiSaitoJapan@users.noreply.github.com>
2019-12-24 08:42:28 +0900

    Delete convex_sensor.m

--- Determinant-based Fast Greedy Sensor Selection Algorithm/convex_sensor.m
+++
@@ -1,5 +0,0 @@
-function [time_convex, H, sensors]=convex_sensor(Psi, r, p, n, maxiteration)
-         tic;
-         [sensors,~]=sensor_convex(Psi(:,1:r), r, p,maxiteration);
-         time_convex=toc;
-         [H]=makesensor_matrix(p, n, sensors);
